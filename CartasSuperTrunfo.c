c42dace76b95af431c7014fffc28bf95e840c149
CleoguimeSouza <cleoguimesouza988@gmail.com>
2026-03-03 17:16:47 +0000

    criei printf e scanf para entrada de dados.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,51 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+    char estado, NomeDaCidade[20];
+    int populacao, codigoDaCarta, PontosTuristico;
+    float area, pib; 
 
   // Área para entrada de dados
 
+
+      printf ("carta 01:\n");
+        printf ("Digite o nome do estado? ");
+          scanf ("%c", &estado);
+
+        printf ("digite o código da carta? ");
+          scanf ("%d", &codigoDaCarta); 
+
+        printf ("digite o nome da cidade? ");
+          scanf ("%s", &NomeDaCidade); 
+
+        printf ("digite a quantidade de população exixtente? ");
+         scanf ("%d", &populacao); 
+
+        printf ("Qual a área do estado? "); 
+         scanf ("%f", &area); 
+
+        printf ("Qual o pib do estado? "); 
+         scanf ("%f", &pib); 
+
+        printf ("Quantos pontos turísticos o estado tem?");
+          scanf ("%d", &PontosTuristico); 
+
   // Área para exibição dos dados da cidade
+
+        printf ("O nome do estado é: %c\n", estado);
+
+        printf ("O codigo da carta é: %d\n", codigoDaCarta);
+
+        printf ("O nome da cidade é: %s\n", NomeDaCidade);
+
+        printf ("A quantidade de pessoas são: %d\n", populacao);
+
+        printf ("A area do estado é: %.2f KM²\n", area);
+
+        printf ("O PIB do estado é: %.2f\n", pib);
+
+        printf ("A quantidade de pontos turistico é: %d\n", PontosTuristico);
+
 
 return 0;
 } 
